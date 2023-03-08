06707f6e009f50df138657b53fb9edc206f1b133
varela-lang <80700042+varela-lang@users.noreply.github.com>
2023-03-08 11:38:07 -0600

    Add files via upload

---
+++ ejercicio de lista/ejercicio_de_lista.js
@@ -0,0 +1,16 @@
+const lista = new Array(4);
+
+lista[0] = "Sergio Varela";
+lista[1] = "edad " + 18;
+lista[2] = "Eres desarrollador " + true;
+lista[3] = new Date( 2005, 0, 1);
+lista[4] = Object.libro
+
+const libro = {
+    titulo: "El principito",
+    autor: "Antoine de Saint Exupéry",
+    fecha: new Date(1943, 3, 6),
+    url: "https://es.wikipedia.org/wiki/El_principito"
+}
+
+console.log(lista)
