912ea9d7d0eec10be1b88e4c2c90a5ccedf9b9e9
varela-lang <80700042+varela-lang@users.noreply.github.com>
2023-04-04 16:06:40 -0600

    Add files via upload

---
+++ sesion5-Numbers/ejercicio5.js
@@ -0,0 +1,13 @@
+let altura = 170;
+let alturad = 1.70;
+
+let peso = 63.5;
+
+let alturac = Math.ceil(alturad);
+console.log(alturac)
+
+let pesoa = Math.floor(peso);
+console.log(pesoa);
+
+let maximo = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+console.log(maximo);
